c94ef9afdd709c92b0cabb69d83b128d990d9910
bruh-pilar <bruna.m,pilar@gmail.com>
2026-05-27 21:11:06 -0300

    16ex.py

---
+++ 16ex.py
@@ -0,0 +1,9 @@
+faixa=int(input("digite sua idade"))
+if  0<= faixa <= 10:
+    print("faixa entre 0 e 10")
+elif 11 <= faixa <= 20:
+    print("faixa entre 11 e 20")
+elif 21<= faixa >= 30:
+    print("faixa entre 21 e 30")
+else:
+    print("maior que 30")
